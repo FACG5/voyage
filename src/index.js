e5cc1ee08f42a47ea42fa8b5ff2557cc7155211d
asmaa-ezz <you@example.com>
2018-09-09 13:50:12 +0300

    add file structure Related #8

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 4000;
+const host = process.env.HOST || 'localhost';
+
+app.listen(PORT, () => {
+    console.log(`the hostname ${host} on port ${PORT}`);
+});
+
+module.exports = app;
