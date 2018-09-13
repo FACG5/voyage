36123156ef2fc6aa3c19557355aa3bb0df94c71c
asmaa-ezz <you@example.com>
2018-09-13 09:36:59 +0300

    Edit the name in table in DB Relates #22

--- src/model/queries/review.js
+++ src/model/queries/review.js
@@ -13,9 +13,9 @@ const getReviews = () => new Promise((resolve, reject) => {
 const getReviewsByBusiness = businessId => new Promise((resolve, reject) => {
   const sql = {
     text: 'SELECT person.username, allDataReview.* FROM '
-      + '(SELECT review.*,avgEval.avg FROM '
+      + '(SELECT review.*,eval.avg FROM '
       + '(SELECT business_id,ROUND(AVG(evaluation)) AS avg FROM review GROUP BY business_id)'
-      + 'avgEval JOIN review on review.business_id=avgEval.business_id WHERE review.business_id=$1)'
+      + 'eval JOIN review on review.business_id=eval.business_id WHERE review.business_id=$1)'
       + 'allDataReview LEFT JOIN person ON person.id = allDataReview.person_id',
     values: [businessId],
   };
