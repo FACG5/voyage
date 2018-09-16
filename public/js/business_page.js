c5f4a7b4d62334a5afef71e43c948bff3e3f17fa
donia98 <nadooshah98@gmail.com>
2018-09-16 11:56:35 +0300

    business page review form

--- public/js/business_page.js
+++ public/js/business_page.js
@@ -17,3 +17,18 @@ aboutButton.addEventListener('click', () => {
   aboutDiv.style.display = 'block';
   reviewsDiv.style.display = 'none';
 });
+ 
+sendBtn.addEventListener('click', () => {
+ document.sendBtn.value = {};
+ fetch('/reviews', {
+   method: 'POST',
+   credentials: 'same-origin',
+   headers: { 'Content-Type': 'application/json; charset=utf-8' },
+   body: JSON.stringify(object),
+ })
+   .then(response => response.json())
+   .then((response) => { 
+    
+       console.log("Pass");   })
+   .catch((error) => { messageError.textContent = error; });
+});
