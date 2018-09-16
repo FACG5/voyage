a5297805416c540cdd7be65696ef53ac52ce4a1c
donia98 <nadooshah98@gmail.com>
2018-09-16 10:57:37 +0300

    delete commands from db_build file

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -45,30 +45,3 @@ CREATE TABLE comment (
 );
 
 COMMIT ;
-
-INSERT INTO users (email, password, type) VALUES ('asmaa@gmail.com', '000', 'person');
-INSERT INTO users (email, password, type) VALUES ('ahmad@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('moh@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('donia@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('waleed@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('emad@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('mat@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('ali@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('asma@gmail.com', '000', 'business');
-INSERT INTO users (email, password, type) VALUES ('asala@gmail.com', '000', 'business');
-
-
-INSERT INTO person (user_id, username, first_name, last_name, birthday, gender) VALUES (1,'asmaa','asmaa','izz', '1996-05-17','femail');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (2,'ahmad','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','restaurant');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (3,'moh','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','cafe');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (4,'donia','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','park');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (5,'waleed','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','park');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (6,'emad','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','restaurant');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (7,'mat','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','cafe');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (8,'ali','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','cafe');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (9,'asma','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','restaurant');
-INSERT INTO business (user_id, name, address, description, img, category) VALUES (10,'asala','Gaza-Naser','There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour', 'http://www.lakeheart.it/files/20170324_185419-1.jpg','park');
-
-INSERT INTO review (person_id, business_id, content, evaluation) VALUES (1, 1, 'There are many variations of passages of Lorem ',4);
-
-INSERT INTO comment (person_id, review_id, content) VALUES (1, 1, 'WooW');
