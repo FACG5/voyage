76dfd823d59489ee6660489a88caabd97a08f736
donia98 <nadooshah98@gmail.com>
2018-09-17 14:12:55 +0300

    delete review form and solve issues

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,4 +1,5 @@
-const catResult = document.querySelector('.catResult');
+/* global document window */
+const catResult = document.querySelector('.cat-result');
 
 catResult.addEventListener('click', (e) => {
   const name = e.target.id;
