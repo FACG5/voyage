dc1bab39c55e6bf9c8e91bbaba801ec7ce928d58
donia98 <nadooshah98@gmail.com>
2018-09-18 11:30:44 +0300

    view the avg in categories page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const router = express.Router();
+
 const categories = require('./categories');
 const home = require('./home');
 const signUp = require('./sign_up');
 const signIn = require('./sign_in');
-
+const signOut = require('./sign_out');
 const business = require('./business_page');
 const error = require('./error');
-const signOut = require('./sign_out');
 const contact = require('./contact_us');
 const about = require('./about_us');
 
@@ -18,17 +18,16 @@ const checkauth = require('../middlewares/checkauth');
 router.use(cookieParser());
 router.use(checkauth.isSingIn);
 
-
 router.get('/', home.get); // router [/] Home Page
-router.post('/', home.post);
 
+router.post('/', home.post);
 
 router.get('/contact_us', contact.get);
 
 router.get('/about_us', about.get);
 
-router.get('/sign_up', signUp.get);
 router.get('/sign_up', signUp.get); // router sign_up Page (git)
+router.post('/sign_up', signUp.post);
 
 router.get('/sign_in', signIn.get); // router sign_in Page (git)
 router.post('/sign_in', signIn.post); // router sign_in Page (post)
