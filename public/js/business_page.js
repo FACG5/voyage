40e92790c69a67128d876c38dc685324c2d1ac7a
donia98 <nadooshah98@gmail.com>
2018-09-18 11:42:04 +0300

    solve conflicts

--- public/js/business_page.js
+++ public/js/business_page.js
@@ -5,8 +5,6 @@ const reviewsButton = getElement('reviews-button');
 const aboutButton = getElement('about-button');
 const reviewsDiv = getElement('reviews-div');
 const aboutDiv = getElement('about-div');
-const reviewInput = require('review-input');
-const rateInput = require('rate-input');
 
 aboutDiv.style.display = 'none';
 
@@ -18,20 +16,4 @@ reviewsButton.addEventListener('click', () => {
 aboutButton.addEventListener('click', () => {
   aboutDiv.style.display = 'block';
   reviewsDiv.style.display = 'none';
-});
-
-sendBtn.addEventListener('click', () => {
-  const object = { reviewInput: reviewInput.value, rateInput: rateInput.value };
-
-  fetch('/reviews', {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: { 'Content-Type': 'application/json; charset=utf-8' },
-    body: JSON.stringify(object),
-  })
-    .then(response => response.json())
-    .then((response) => {
-      console.log('Pass');
-    })
-    .catch((error) => { messageError.textContent = error; });
 });
