5d32bf5adee8e66e153472f488a9e1198d5c5ab0
asmaa-ezz <you@example.com>
2018-09-18 13:56:14 +0300

    fix

--- src/controllers/sign_up.js
+++ src/controllers/sign_up.js
@@ -8,7 +8,7 @@ exports.get = (req, res) => {
     res.redirect('/');
   } else {
     res.render('sign_up', {
-      style: 'style', dom: 'sign_up', title: 'sign up', vald: 'validation'
+      style: 'style', dom: 'sign_up', title: 'sign up', vald: 'validation',
     });
   }
 };
@@ -24,14 +24,14 @@ exports.post = (request, response) => {
         addUser(data, hash)
           .then((result) => {
             const userId = result.rows[0].id;
-            if (type === 0) { // type = 0 represents person
+            if (type === 'person') { // type = 0 represents person
               addPerson(userId, data)
                 .then(() => {
                   response.send({ message: 'Person has been added successful', pass: true });
                 }).catch(() => {
                   response.send({ message: ' username already token', pass: false });
                 });
-            } else if (type === 1) { // type = 1 represents business
+            } else if (type === 'business') { // type = 1 represents business
               addBusiness(userId, data)
                 .then(() => {
                   response.send({ message: 'Business has been added successful', pass: true });
