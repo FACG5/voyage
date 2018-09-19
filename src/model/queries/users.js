4d9495810c3be28393fdebcdde76801653016908
ahmad1091 <ahmadtayeb91@gmail.com>
2018-09-19 12:45:11 +0300

    solve conflict

--- src/model/queries/users.js
+++ src/model/queries/users.js
@@ -13,6 +13,31 @@ const checkUser = email => new Promise((resolve, reject) => {
   });
 });
 
+const getUserData = username => new Promise((resolve, reject) => {
+  const sql = {
+    text: 'SELECT person.* , users.email FROM person JOIN users ON person.user_id = users.id WHERE username=$1',
+    values: [username],
+  };
+  dbConnection.query(sql, (error, res) => {
+    if (error) {
+      return reject(new Error('Error in DB'));
+    }
+    return resolve(res.rows);
+  });
+});
+ const getReviewByUser = username => new Promise((resolve, reject) => {
+  const sql = {
+    text: 'SELECT person.user_id, person.username, review.content, review.evaluation, business.user_id, business.name FROM person JOIN review ON person.id = review.person_id JOIN business ON  business.id= review.business_id WHERE person.username=$1',
+    values: [username],
+  };
+  dbConnection.query(sql, (error, res) => {
+    if (error) {
+      return reject(new Error('Error in DB'));
+    }
+    return resolve(res.rows);
+  });
+});
+
 const addUser = (data, hash) => new Promise((resolve, reject) => {
   const { email, type } = data;
   const sql = {
@@ -55,6 +80,8 @@ const getName = (type, id) => new Promise((resolve, reject) => {
 
 module.exports = {
   checkUser,
+  getUserData,
+  getReviewByUser,
   addUser,
   getName,
 };
